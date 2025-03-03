7beb5d5a98c9ef7dd71872e2cb97f5f5dddd0554
kvtodev <kvtodev@outlook.com>
2025-03-03 21:45:54 +0800

    add qt for gui

--- package.sh
+++ package.sh
@@ -1,4 +1,3 @@
 pandoc doc.md -f markdown -t pdf -o doc.pdf --pdf-engine=xelatex -V mainfont='JetBrains Mono' -V CJKmainfont='Noto Serif CJK SC'
-#tar -cf homework.tar CMakeLists.txt doc.md doc.pdf include/ src/ tests/ application/
-zip -r  homework.zip CMakeLists.txt doc.md doc.pdf package.sh include/ src/ tests/ application/
+zip -r  homework.zip CMakeLists.txt doc.md doc.pdf package.sh include/ src/ tests/ console_application/ gui_application/
 cp homework.zip /mnt/c/Users/kvtoD/Downloads/王德宇-2024013267.zip
