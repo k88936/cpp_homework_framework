50c28ca2b7475c432343d71dbcab4cb45b3f1d41
kvtodev <kvtodev@outlook.com>
2025-03-23 20:11:44 +0800

    more powerful assert

--- tests/utils.h
+++ tests/utils.h
@@ -9,6 +9,6 @@
 //#define TEST(func) int result_##func = func();if(result_##func)std::cout<<#func<<" failed"<<std::endl;else std::cout<<#func<<" passed"<<std::endl;
 //#define TEST_END return 0;}
 
-#define ASSERT(expr) if(!(expr)){std::cout<<"assert: "<<#expr<<" failed"<<std::endl; return 1;}
+#define assert(expr1,expr2) {auto __1 = expr1;auto __2 = expr2;if(__1 == __2 ){std::cout<<"assert: "<<#expr1<<" equals " <<__2<<" passed"<<std::endl;}else{std::cout<<"assert failed: expected: "<<__2<<" but got: "<<__1<<std::endl; return 1;}}
 
 #endif //HOMEWORK_UTILS_H
