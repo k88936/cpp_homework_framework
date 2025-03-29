c240ea0ea1a2c6edf02668e1a61cced7cfa9cec9
kvtodev <kvtodev@outlook.com>
2025-03-29 16:44:45 +0800

    better assert and better gui main

--- gui_application/main.cpp
+++ gui_application/main.cpp
@@ -1,12 +1,11 @@
 #include <QApplication>
-#include <QPushButton>
-#include "lib.h"
 #include "mywidget.h"
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    mywidget w;
-    w.setWindowTitle("gui_application");
-    w.show();
-    return QApplication::exec();
+    QApplication app(argc, argv);
+
+    mywidget widget;
+    widget.show();
+
+    return app.exec();
 }
