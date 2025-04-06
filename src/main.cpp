cf392aaf7f80cf64d35b38a397be59aa7bd9508c
kvtodev <kvtodev@outlook.com>
2025-04-06 17:09:36 +0800

    use ipc instead

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//
+// Created by root on 4/6/25.
+//
+int main(int argc, char **argv) {
+    std::cin >> argc;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << argc;
+}
